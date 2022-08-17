1fbb63b5755b3d503fbcaf6531518453a93074f6
Leandro Ferreira <leandro@vipsolucoes.com>
2022-08-17 09:11:25 -0300

    Commit inicial

---
+++ Vip.ListaIBPT/Ncm.cs
@@ -0,0 +1,53 @@
+﻿using FileHelpers;
+
+namespace Vip.ListaIBPT;
+
+[DelimitedRecord(";")]
+[IgnoreFirst(1)]
+public class Ncm : IEquatable<Ncm>
+{
+    #region Propriedades
+
+    public string Codigo { get; set; }
+    public string Ex { get; set; }
+    public int Tipo { get; set; }
+    [FieldQuoted] public string Descricao { get; set; }
+
+    [FieldConverter(ConverterKind.Decimal, ".")]
+    public decimal NacionalFederal { get; set; }
+
+    [FieldConverter(ConverterKind.Decimal, ".")]
+    public decimal ImportadoFederal { get; set; }
+
+    [FieldConverter(ConverterKind.Decimal, ".")]
+    public decimal Estadual { get; set; }
+
+    [FieldConverter(ConverterKind.Decimal, ".")]
+    public decimal Municipal { get; set; }
+
+    [FieldConverter(ConverterKind.Date, "dd/MM/yyyy")]
+    public DateTime VigenciaInicio { get; set; }
+
+    [FieldConverter(ConverterKind.Date, "dd/MM/yyyy")]
+    public DateTime VigenciaFim { get; set; }
+
+    public string Chave { get; set; }
+    public string Versao { get; set; }
+    public string Fonte { get; set; }
+
+    #endregion
+
+    #region Métodos
+
+    public bool Equals(Ncm other)
+    {
+        return Codigo == other?.Codigo;
+    }
+
+    public override int GetHashCode()
+    {
+        return Codigo.GetHashCode();
+    }
+
+    #endregion
+}
---
+++ Vip.ListaIBPT/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Globalization;
+using System.Text;
+using FileHelpers;
+using Vip.ListaIBPT;
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+ImprimeLinha("Projeto para converter lista IBPT de csv para instrução SQL", 2);
+Console.ForegroundColor = ConsoleColor.White;
+
+ImprimeLinha("Informe o caminho do arquivo CSV");
+Console.Write("> ");
+var caminhoArquivo = Console.ReadLine();
+
+if (caminhoArquivo.IsNullOrEmpty() || !File.Exists(caminhoArquivo))
+{
+    ImprimeVermelho($"Arquivo {caminhoArquivo} não encontrado! Por favor, tente novamente");
+    return;
+}
+
+ImprimeVerde($"Arquivo {caminhoArquivo} encontrado com sucesso!", 2);
+ImprimeLinha("Aguarde um momento... arquivo será processado!");
+
+// é necessário converter o arquivo original para UTF8
+var engine = new FileHelperEngine<Ncm>(Encoding.UTF8);
+var registros = engine.ReadFile(caminhoArquivo).Where(x => x.Tipo == 0 && x.Codigo.Length == 8).Distinct().ToList();
+
+if (registros.IsNull() || registros.IsEmpty())
+{
+    ImprimeVermelho("Nenhuma informação encontrada. Tente novamente");
+    return;
+}
+
+const int totalLinhas = 240;
+var linha = 1;
+var totalArquivo = 1;
+
+var arquivo = new StringBuilder();
+arquivo.AppendLine(MontarCabecalho());
+foreach (var registro in registros)
+{
+    arquivo.AppendLine(MontarLinha(registro, linha.Equals(totalLinhas) || totalArquivo.Equals(registros.Count)));
+
+    if (linha.Equals(totalLinhas))
+    {
+        arquivo.AppendLine(MontarCabecalho());
+        linha = 1;
+    }
+
+    linha++;
+    totalArquivo++;
+}
+
+File.WriteAllText(@"D:\tabela.sql", arquivo.ToString());
+ImprimeVerde($"Arquivo convertido com sucesso! Total de registros: {registros.Count}");
+
+Console.ReadKey();
+
+#region Funções
+
+string MontarCabecalho()
+{
+    return "INSERT INTO \"Ncm\" (\"NcmId\", \"Descricao\", \"ImpostoFederalNacional\", \"ImpostoFederalImportado\", \"ImpostoEstadual\", \"ImpostoMunicipal\", \"Inativo\") VALUES ";
+}
+
+string MontarLinha(Ncm ncm, bool ultimaLinha)
+{
+    var numberFormat = new NumberFormatInfo {NumberDecimalSeparator = "."};
+
+    var registro = $"('{ncm.Codigo}','{ncm.Descricao}',{ncm.NacionalFederal.ToString(numberFormat)},{ncm.ImportadoFederal.ToString(numberFormat)},{ncm.Estadual.ToString(numberFormat)},{ncm.Municipal.ToString(numberFormat)},0)";
+    registro += ultimaLinha ? "; \r\n" : ",";
+    return registro;
+}
+
+void ImprimeLinha(string texto, int pularLinha = 0)
+{
+    Console.WriteLine(texto);
+
+    if (pularLinha > 0)
+        for (var i = 0; i < pularLinha; i++)
+            Console.WriteLine(string.Empty);
+}
+
+void ImprimeVerde(string texto, int pularLinha = 1)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    ImprimeLinha(texto, pularLinha);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+void ImprimeVermelho(string texto, int pularLinha = 1)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    ImprimeLinha(texto, pularLinha);
+    Console.ForegroundColor = ConsoleColor.White;
+}
+
+#endregion
